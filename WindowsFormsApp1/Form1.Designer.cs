d04dff00fe6ba0d8ef69d21eee76c90c68cdbb12
izuna <reddevil241097@gmail.com>
2020-10-27 17:25:59 +0700

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Đổi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "https://images-na.ssl-images-amazon.com/images/I/51Lt7h7nReL._AC_SX466_.jpg";
+            this.pictureBox1.Location = new System.Drawing.Point(257, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(279, 349);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private readonly string[] urls = {
+        "https://nghesiviet.vn/storage/files/7/phuongly/phuong-ly.jpg",
+        "https://cdn.24h.com.vn/upload/4-2018/images/2018-12-14/Thu-ngay-nhung-goi-y-ve-toc-va-da-cua-Phuong-Ly-cho-mua-le-hoi-an-tuong-1-1544771307-522-width660height880.jpg",
+        "https://www.hellolamode.com/wp-content/uploads/2018/06/a-6.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThrvMDaftX1o4OgWj-pGwwx6fzaTWb9-zyTA&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSZ8aogCck8Jap9fZdHNLbFqy2xlJX1KHRgcw&usqp=CAU",
+        "https://s1.img.yan.vn/YanNews/2167221/201610/20161023-070432-1y4a0094-copy_600x900.jpg",
+        "https://vcdn1-ione.vnecdn.net/2018/10/15/hot-girl-phuong-ly-10-8232-1539591271.jpg?w=460&h=0&q=100&dpr=1&fit=crop&s=oorVyvXlRlavQOO6AogBSA",
+        "https://i.vietgiaitri.com/2019/4/19/thoat-nong-em-gai-phuong-linh-phuong-ly-tu-tin-bien-hoa-tu-cong--1eb72c.jpg",
+        "https://kenh14cdn.com/thumb_w/660/2019/1/16/hoatran-7198-15476571059471523863510.jpg",
+        "https://cdn.pose.com.vn/assets/2019/02/27/may_lily_47689998_2003041526662308_4356240748385774162_n.jpg"};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ran = new Random().Next(0, 10);
+            pictureBox1.Load(urls[ran]);
+        }
+    }
+}
